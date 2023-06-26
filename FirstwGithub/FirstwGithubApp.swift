69289abb41f02dc16116541cbb9e362d9cd5632d
Anna Chan <a.m.chan.1210@gmail.com>
2023-06-26 10:30:11 -0700

    Initial Commit

---
+++ FirstwGithub/FirstwGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstwGithubApp.swift
+//  FirstwGithub
+//
+//  Created by Choon-Weng Chan on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstwGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
